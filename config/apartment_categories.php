adf8446bc29a495c6bf465badb63dbe7acf6a417
Riccardo Basso <riccardo.basso2001@gmail.com>
2023-09-26 19:34:40 +0200

    Added category and update navbar

--- config/apartment_categories.php
+++ config/apartment_categories.php
@@ -1,7 +1,239 @@
 <?php
 return [
-    ['name' => 'Casa', 'icon' => 'house'],
-    ['name' => 'Appartamento', 'icon' => 'couch'],
-    ['name' => 'Hotel', 'icon' => 'hotel'],
-    ['name' => 'Pensione', 'icon' => 'bed']
+    [
+        'label' => 'Wow!',
+        'img' => 'wow.jpg',
+    ],
+    [
+        'label' => 'Piscine incredibili',
+        'img' => 'pool.jpg',
+    ],
+    [
+        'label' => 'Di tendenza',
+        'img' => 'trend.jpg',
+    ],
+    [
+        'label' => 'Minicase',
+        'img' => 'mini-houses.jpg',
+    ],
+    [
+        'label' => 'Grotte',
+        'img' => 'caves.jpg',
+    ],
+    [
+        'label' => 'sulle piste',
+        'img' => 'on-slopes.jpg',
+    ],
+    [
+        'label' => 'Baite',
+        'img' => 'cabins.jpg',
+    ],
+    [
+        'label' => 'Vista mozzafiato',
+        'img' => 'nice-view.jpg',
+    ],
+    [
+        'label' => 'Ville',
+        'img' => 'villas.jpg',
+    ],
+    [
+        'label' => 'Cupole',
+        'img' => 'domes.jpg',
+    ],
+    [
+        'label' => 'Luxe',
+        'img' => 'luxe.jpg',
+    ],
+    [
+        'label' => 'Case sull\'albero',
+        'img' => 'on-trees.jpg',
+    ],
+    [
+        'label' => 'Barche',
+        'img' => 'boats.jpg',
+    ],
+    [
+        'label' => 'Design',
+        'img' => 'design.jpg',
+    ],
+    [
+        'label' => 'Castelli',
+        'img' => 'castles.jpg',
+    ],
+    [
+        'label' => 'Sull\'acqua',
+        'img' => 'on-water.jpg',
+    ],
+    [
+        'label' => 'Luoghi remoti',
+        'img' => 'remote-places.jpg',
+    ],
+    [
+        'label' => 'Spiaggia',
+        'img' => 'beach.jpg',
+    ],
+    [
+        'label' => 'Vigneti',
+        'img' => 'vineyards.jpg',
+    ],
+    [
+        'label' => 'Dimore storiche',
+        'img' => 'historic-homes.jpg',
+    ],
+    [
+        'label' => 'Lago',
+        'img' => 'lake.jpg',
+    ],
+    [
+        'label' => 'Agriturismi',
+        'img' => 'farmhouses.jpg',
+    ],
+    [
+        'label' => 'Campagna',
+        'img' => 'countryside.jpg',
+    ],
+    [
+        'label' => 'Case galleggianti',
+        'img' => 'floating-houses.jpg',
+    ],
+    [
+        'label' => 'B&B',
+        'img' => 'b&b.jpg',
+    ],
+    [
+        'label' => 'Parchi nazionali',
+        'img' => 'national-parks.jpg',
+    ],
+    [
+        'label' => 'isole',
+        'img' => 'islands.jpg',
+    ],
+    [
+        'label' => 'Città popolari',
+        'img' => 'popolar-cities.jpg',
+    ],
+    [
+        'label' => 'Stanze',
+        'img' => 'rooms.jpg',
+    ],
+    [
+        'label' => 'A-frame',
+        'img' => 'a-frame.jpg',
+    ],
+    [
+        'label' => 'Torri',
+        'img' => 'towers.jpg',
+    ],
+    [
+        'label' => 'Fronte lago',
+        'img' => 'lake-front.jpg',
+    ],
+    [
+        'label' => 'Tropicali',
+        'img' => 'tropical.jpg',
+    ],
+    [
+        'label' => 'Camping',
+        'img' => 'camping.jpg',
+    ],
+    [
+        'label' => 'Spazi per giocare',
+        'img' => 'games.jpg',
+    ],
+    [
+        'label' => 'Novità',
+        'img' => 'news.jpg',
+    ],
+    [
+        'label' => 'Fienili',
+        'img' => 'barns.jpg',
+    ],
+    [
+        'label' => 'Case organiche',
+        'img' => 'organic-homes.jpg',
+    ],
+    [
+        'label' => 'Mulini',
+        'img' => 'mills.jpg',
+    ],
+    [
+        'label' => 'Surf',
+        'img' => 'surf.jpg',
+    ],
+    [
+        'label' => 'In cima al mondo',
+        'img' => 'top-world.jpg',
+    ],
+    [
+        'label' => 'Polo Nord',
+        'img' => 'north-pole.jpg',
+    ],
+    [
+        'label' => 'Trulli',
+        'img' => 'trulli.jpg',
+    ],
+    [
+        'label' => 'Case cicladiche',
+        'img' => 'cycladic-houses.jpg',
+    ],
+    [
+        'label' => 'Pianoforti a coda',
+        'img' => 'piano.jpg',
+    ],
+    [
+        'label' => 'Spazi creativi',
+        'img' => 'creative-spaces.jpg',
+    ],
+    [
+        'label' => 'Cucine professionali',
+        'img' => 'kitchens.jpg',
+    ],
+    [
+        'label' => 'Case particolari',
+        'img' => 'particular-houses.jpg',
+    ],
+    [
+        'label' => 'Nel deserto',
+        'img' => 'desert.jpg',
+    ],
+    [
+        'label' => 'Ryokan',
+        'img' => 'ryokan.jpg',
+    ],
+    [
+        'label' => 'Container',
+        'img' => 'container.jpg',
+    ],
+    [
+        'label' => 'Capanne',
+        'img' => 'desert.jpg',
+    ],
+    [
+        'label' => 'Spazi accessibili',
+        'img' => 'accessible-spaces.jpg',
+    ],
+    [
+        'label' => 'Riad',
+        'img' => 'riad.jpg',
+    ],
+    [
+        'label' => 'Minsu',
+        'img' => 'minsu.jpg',
+    ],
+    [
+        'label' => 'Golf',
+        'img' => 'golf.jpg',
+    ],
+    [
+        'label' => 'Hanok',
+        'img' => 'hanok.jpg',
+    ],
+    [
+        'label' => 'Camper',
+        'img' => 'camper.jpg',
+    ],
+    [
+        'label' => 'Sci',
+        'img' => 'sci.jpg',
+    ]
 ];
--- database/migrations/2023_09_26_092320_create_categories_table.php
+++ database/migrations/2023_09_26_092320_create_categories_table.php
@@ -14,8 +14,8 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
 
-            $table->string('name', 25);
-            $table->text('icon')->nullable();
+            $table->string('label', 25);
+            $table->text('img')->nullable();
 
             $table->timestamps();
         });
