1f86271b619bb9e37c485c73e85b00d647e1148e
Riccardo Basso <riccardo.basso2001@gmail.com>
2023-09-29 12:27:35 +0200

    move logout button to admin home

--- resources/views/includes/header.blade.php
+++ resources/views/includes/header.blade.php
@@ -95,13 +95,6 @@ $firstLetter = strtoupper(substr($name, 0, 1));
                             <li><a class="dropdown-item" href="{{ route('admin.apartments.create') }}">Apri un Boolbnb</a>
                             </li>
                             <li><a class="dropdown-item" href="{{ url('profile') }}">Account</a></li>
-                            <hr>
-                            <li><a class="dropdown-item" href="{{ route('logout') }}"
-                                    onclick="event.preventDefault();  document.getElementById('logout-form').submit();">Esci</a>
-                            </li>
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                @csrf
-                            </form>
                         @endguest
                     </ul>
                 </div>
