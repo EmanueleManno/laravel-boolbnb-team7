ad812aedacfd514f4e8452fbb28c043914127ec5
EmanueleManno <mannus1994@gmail.com>
2023-10-01 18:07:53 +0200

    function index in MessageController (comment)

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -2,9 +2,30 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Message;
 use Illuminate\Http\Request;
 
 class MessageController extends Controller
 {
+    // public function index()
     //
+    //{
+    //    return view('message.index');
+    //}
+    //
+    //public function store(Request $request)
+    //
+    //{
+    //    $request->validate([
+    //        'name' => 'required',
+    //        'email' => 'required|email',
+    //        'content' => 'required'
+    //    ]);
+    //
+    //    Message::create($request->all());
+    //
+    //    return redirect()->back()
+
+    //        ->with(['success' => 'Grazie per aver mandato il messaggio']);
+    //}
 }
