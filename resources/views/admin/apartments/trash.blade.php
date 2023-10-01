51792890a55d9cd2b23588acafe1f6c7eac00126
EmanueleManno <mannus1994@gmail.com>
2023-10-01 09:32:11 +0200

    trash fix

--- resources/views/admin/apartments/trash.blade.php
+++ resources/views/admin/apartments/trash.blade.php
@@ -16,14 +16,21 @@
             <div>
 
                 {{-- Back --}}
-                <a href="{{ route('admin.apartments.index') }}" class="btn btn-secondary">Torna alla Lista</a>
+                <a href="{{ route('admin.apartments.index') }}" class="btn btn-secondary">
+                    <span class="d-none d-md-flex">Torna alla Lista</span>
+                    <i class="d-inline-block d-md-none fa-solid fa-backward"></i>
+                </a>
+                
 
                 {{-- Drop All --}}
                 <form class="delete-form ms-2 delete-all d-inline-block" method="POST"
                     action="{{ route('admin.apartments.dropAll') }}" data-bs-toggle="modal" data-bs-target="#deleteModal">
                     @csrf
                     @method('DELETE')
-                    <button class="btn btn-danger">Svuota cestino</button>
+                    <button class="btn btn-danger">
+                        <span class="d-none d-md-flex">Svuota cestino</span>
+                        <i class="d-inline-block d-md-none fa-solid fa-broom"></i>
+                    </button>
                 </form>
             </div>
 
