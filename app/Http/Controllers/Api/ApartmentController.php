8336258c4386200a725e9ae23804bedea90928bd
l3on <l3onwebdev@gmail.com>
2023-10-01 10:44:39 +0200

    added filter function to apartments index

--- app/Http/Controllers/Api/ApartmentController.php
+++ app/Http/Controllers/Api/ApartmentController.php
@@ -5,18 +5,33 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Apartment;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class ApartmentController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        $apartemnts = Apartment::where('is_visible', true)
-            ->orderBy('updated_at', 'DESC')
-            ->get();
-        return response()->json($apartemnts);
+
+        // Get filters
+        $filters = $request->all();
+
+
+        if (isset($filters['lat']) && isset($filters['lon'])) {
+
+            // Get filtered apartments
+            $apartments = $this->filterApartments($filters);
+        } else {
+
+            // Get all apartments
+            $apartments = Apartment::where('is_visible', true)
+                ->orderBy('updated_at', 'DESC')
+                ->get();
+        }
+
+        return response()->json($apartments);
     }
 
     /**
@@ -49,5 +64,28 @@ class ApartmentController extends Controller
     public function destroy(string $id)
     {
         //
+    }
+
+    /**
+     * Filter the specified resource
+     */
+    public function filterApartments($filters)
+    {
+        // API Data
+        $api_uri = 'https://api.tomtom.com/search/2/geometryFilter.json';
+        $api_key = config('api_config.tt_key');
+
+        // Get all visible apartments and apply other filters (TODO)
+        $apartments = Apartment::where('is_visible', 1)->get();
+
+        // Fetch API
+        $response = Http::get($api_uri, [
+            'key' => $api_key
+        ]);
+
+
+
+        return $apartments; //! DEBUG
+        //return $apartments;
     }
 }
