96e8cc74cb02ffb79d2e822b91f159cfc21ce58f
EmanueleManno <mannus1994@gmail.com>
2023-10-02 16:12:38 +0200

    message index.blade draft

--- resources/views/message/index.blade.php
+++ resources/views/message/index.blade.php
@@ -1,18 +1,82 @@
-<!--<div class="container">
-<form>
-    <div class="form-group">
-        <label for="exampleInputEmail1">Indirizzo Email</label>
-        <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Inserisci la mail">
-    </div>
-    <div class="form-group">
-        <label for="exampleFormControlTextarea1">Titolo del messaggio</label>
-        <input type="text" class="form-control" id="exampleFormControlTextarea1" placeholder="Inserisci il titolo">
-    </div>
-    <div class="form-group">
-        <label for="exampleFormControlTextarea2">Contenuto del messaggio</label>
-        <input type="text" class="form-control" id="exampleFormControlTextarea2" placeholder="Inserisci il contenuto">
-    </div>
-    <button type="submit" class="btn btn-primary">Invia messaggio</button>
-</form>
-</div>-->
+@extends('layouts.app')
 
+@section('title', 'Messages')
+
+@section('main')
+
+    <div class="container my-5">
+
+        <!--Header-->
+        <header class="d-flex align-items-center justify-content-between pb-4">
+
+            {{-- Page Title --}}
+            <h2>Lista Messaggi</h2>
+
+            {{-- Page Actions --}}
+            <div>
+                <a href="{{ route('message.create') }}" class="btn btn-success">
+                    <span class="d-none d-md-flex">
+                       Aggiungi nuovo messaggio
+                    </span>
+                    <i class="d-inline-block d-md-none fa-solid fa-plus"></i>
+                </a>
+            </div>
+
+        </header>
+
+        <table class="table table-white table-hover align-middle mt-5">
+
+            {{-- Table Headers --}}
+            <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Titolo</th>
+                    <th scope="col">Contenuto</th>
+                    <th scope="col">Email</th>
+                    <th scope="col" class="d-none d-lg-table-cell">Data Creazione</th>
+                    <th scope="col" class="d-none d-lg-table-cell">Ultima Modifica</th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+        
+            {{-- Table Body --}}
+            <tbody>
+                @forelse ($messages as $message)
+                    <tr>
+                        {{-- ID --}}
+                        <th scope="row">{{ $message->id }}</th>
+        
+                        {{-- Title --}}
+                        <td>{{ $message->name }}</td>
+
+                        {{-- Content --}}
+                        <td>{{ $message->content }}</td>
+
+                        {{-- Mail --}}
+                        <td>{{ $message->email }}</td>
+        
+                        {{-- Dates --}}
+                        <td class="d-none d-lg-table-cell">{{ $message->created_at }}</td>
+                        <td class="d-none d-lg-table-cell">{{ $message->updated_at }}</td>
+                    </tr>
+        
+                    {{-- Empty message --}}
+                @empty
+                    <tr>
+                        <td class="text-center" colspan="8">
+                            <h3>Non ci sono messaggi</h3>
+                        </td>
+                    </tr>
+                @endforelse
+            </tbody>
+        </table>
+        
+
+        {{-- Alerts --}}
+        <div class="my-2">
+            @include('includes.alerts')
+        </div>
+
+    </div>
+
+@endsection
