4244ecc4d45cfcd89dab2d4ddb9b4b8a61072a4e
l3on <l3onwebdev@gmail.com>
2023-10-02 17:04:59 +0200

    changed distance calculation

--- app/Http/Controllers/Api/FilterController.php
+++ app/Http/Controllers/Api/FilterController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Apartment;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Http;
 
 class FilterController extends Controller
 {
@@ -14,83 +13,34 @@ class FilterController extends Controller
      */
     public function index(Request $request)
     {
-        // Get Filters
+
+        //*** FILTERS DATA ***//
         $filters = $request->all();
+        $radius = $filters['radius'] ?? 20000;
 
         // Check Required parameters
         if (!isset($filters['lat']) || !isset($filters['lon'])) return response(400);
 
 
-        //*** API DATA ***//
-        $api_uri = 'https://api.tomtom.com/search/2/geometryFilter.json';
-        $api_key = config('api_config.tt_key');
-        $default_radius = 20000; // [m]
-
-
         //*** GET APARTMENTS WITH FILTERS ***//
+        // Get apartments fields and calculate distance
+        $query = Apartment::selectRaw("*, ST_Distance_Sphere(POINT({$filters['lon']}, {$filters['lat']}), POINT( `longitude`, `latitude`)) AS `distance`");
+
         // Get all visible apartments
-        $query = Apartment::where('is_visible', 1);
+        $query->where('is_visible', 1);
 
         // TODO: Filtro "min rooms"
         // TODO: Filtro "min beds"
         // TODO: Filtro "services"
 
+        // Filter by distance and order ASC
+        $query->having('distance', '<', $radius);
+        $query->orderBy('distance');
+
         // Apply query and get apartments
         $apartments = $query->get();
 
 
-        //*** SET TOM TOM API PARAMETERS ***//
-        $radius = $filters['radius'] ?? $default_radius;
-        $request_poi_list = [];
-
-        // Set cirlce geometry
-        $request_geometry_list = [
-            [
-                "type" => "CIRCLE",
-                "position" => "{$filters['lat']}, {$filters['lon']}",
-                "radius" => $radius
-            ]
-        ];
-
-        // Set POI List from apartments
-        foreach ($apartments as $apartment) {
-            $request_poi_list[] = [
-                "poi" => [
-                    "name" => $apartment['id']
-                ],
-                "address" => [
-                    "freeformAddress" => $apartment['address']
-                ],
-                "position" => [
-                    "lat" => $apartment['latitude'],
-                    "lon" => $apartment['longitude']
-                ]
-            ];
-        }
-
-
-        //*** FETCH API ***//
-        $response_poi_list = Http::get($api_uri, [
-            'key' => $api_key,
-            'geometryList' => json_encode($request_geometry_list),
-            'poiList' => json_encode($request_poi_list)
-        ])->json('results');
-
-
-        // TODO: ordinare la lista per distanza ASC
-
-        //*** GET FILTERED APARTMENTS ***//
-        $filtered_apartments = [];
-
-        foreach ($response_poi_list as $poi_data) {
-            // Get ID from poi name
-            $id = $poi_data['poi']['name'];
-
-            // Find apartment
-            $filtered_apartments[] = $apartments->find($id);
-        }
-
-
-        return $filtered_apartments;
+        return $apartments;
     }
 }
