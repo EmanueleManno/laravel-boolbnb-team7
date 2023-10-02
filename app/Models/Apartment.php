4e52f1d90fa4d8b02d1bcced40897d4ddfa3b7a4
Riccardo Basso <riccardo.basso2001@gmail.com>
2023-10-02 16:01:36 +0200

    add getter on Apartment model

--- app/Models/Apartment.php
+++ app/Models/Apartment.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -42,5 +43,13 @@ class Apartment extends Model
     public function messages()
     {
         return $this->hasMany(Message::class);
+    }
+
+    // Getter img
+    public function image()
+    {
+        return Attribute::make(
+            get: fn ($value) => asset('storage/' . $value)
+        );
     }
 }
