5d1ee9463d49f50898ff06c43664b737b36a7a90
Riccardo Basso <riccardo.basso2001@gmail.com>
2023-10-05 17:16:53 +0200

    edit trash

--- resources/views/admin/apartments/trash.blade.php
+++ resources/views/admin/apartments/trash.blade.php
@@ -1,41 +1,40 @@
 @extends('layouts.app')
 
-@section('title', 'trash')
+@section('title', 'Cestino')
 
 @section('main')
 
-    <div class="container my-5">
-
-        <!--Header-->
-        <header class="d-flex align-items-center justify-content-between pb-4">
-
-            {{-- Page Title --}}
-            <h2>Cestino</h2>
-
-            {{-- Page Actions --}}
-            <div>
-
-                {{-- Back --}}
-                <a href="{{ route('admin.apartments.index') }}" class="btn btn-secondary">
-                    <span class="d-none d-md-flex">Torna alla Lista</span>
-                    <i class="d-inline-block d-md-none fa-solid fa-backward"></i>
-                </a>
-                
-
-                <!-- Svuota cestino commentato -->
-                {{-- Drop All --}} 
-                <!--<form class="delete-form ms-2 delete-all d-inline-block" method="POST"
-                    action="{{--{{ route('admin.apartments.dropAll') }}--}}" data-bs-toggle="modal" data-bs-target="#deleteModal">
-                    {{--@csrf
-                    @method('DELETE')--}}
-                    <button class="btn btn-danger">
-                        <span class="d-none d-md-flex">Svuota cestino</span>
-                        <i class="d-inline-block d-md-none fa-solid fa-broom"></i>
-                    </button>
-                </form>-->
-
+    <section id="trash" class="container">
+        {{-- Header --}}
+        <header>
+            {{-- Title --}}
+            <div class="d-flex align-items-center gap-3">
+                <h2>Cestino</h2>
+                <div class="button-primary">
+                    <i class="fa-solid fa-trash-can fa-beat"></i>
+                </div>
             </div>
 
+            <div>
+                {{-- Back Button --}}
+                <div class="go-back">
+                    <a href="{{ route('admin.apartments.index') }}">
+                        <i class="fa-solid fa-chevron-left"></i>
+                    </a>
+                </div>
+                <!-- Svuota cestino commentato -->
+                {{-- Drop All --}}
+                <!--<form class="delete-form ms-2 delete-all d-inline-block" method="POST"
+                                                                                    action="{{-- {{ route('admin.apartments.dropAll') }} --}}" data-bs-toggle="modal" data-bs-target="#deleteModal">
+                                                                                    {{-- @csrf
+                    @method('DELETE') --}}
+                                                                                    <button class="btn btn-danger">
+                                                                                        <span class="d-none d-md-flex">Svuota cestino</span>
+                                                                                        <i class="d-inline-block d-md-none fa-solid fa-broom"></i>
+                                                                                    </button>
+                                                                                </form>-->
+
+            </div>
         </header>
 
         {{-- Alerts --}}
@@ -45,16 +44,13 @@
 
         {{-- Apartment List --}}
         @include('includes.apartments.apartment-list')
-
-    </div>
+    </section>
 
     {{-- Delete Modal --}}
     @include('includes.delete-modal')
-
 @endsection
 
+{{-- Scripts --}}
 @section('scripts')
-
     @vite('resources/js/confirm-delete.js')
-
 @endsection
