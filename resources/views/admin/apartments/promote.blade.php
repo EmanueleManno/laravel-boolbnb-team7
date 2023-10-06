592664b5c04a56c2d752d91f6c2b86188a6d801f
l3on <l3onwebdev@gmail.com>
2023-10-06 18:09:07 +0200

    fixed promote form style

--- resources/views/admin/apartments/promote.blade.php
+++ resources/views/admin/apartments/promote.blade.php
@@ -9,65 +9,45 @@
 @section('main')
     <div class="contrainer p-5">
 
-        <h1 class="mb-5 text-center">Payment</h1>
-        
-        {{-- Payments Form--}}
-        <form id="payment-form" action="{{route('admin.apartments.payment', $apartment)}}" method="post" data-token="{{$clientToken}}">
-        @csrf
+        <h1 class="mb-5">Promuovi il tuo appartamento</h1>
+
+
+        <p class="fs-5 mb-4">{{ $apartment->title }}</p>
+
+        {{-- Payments Form --}}
+        <form id="payment-form" action="{{ route('admin.apartments.payment', $apartment) }}" method="post"
+            data-token="{{ $clientToken }}">
+            @csrf
             {{-- Radios --}}
             <div class="row text-center">
-                
-                {{-- Prom 1--}}
-                <div class="col-4">
-                    <div class="card">
-                        <h2>Prom 1</h2>
-                        <div class="form-check-input d-flex">
-                            <input class="form-check-input" type="radio" name="promotion" id="promotion1" value="1" checked>
-                            <label class="form-check-label" for="promotion1">1</label>
-                        </div>
-                        <p>
-                            Blablabla
-                        </p>
+
+                @foreach ($promotions as $promotion)
+                    {{-- Prom 1 --}}
+                    <div class="col-4">
+                        <label class="card p-2">
+                            <h2>{{ $promotion->name }}</h2>
+                            <div class="d-flex">
+                                <input type="radio" class="invisible" name="promotion" id="promotion-{{ $promotion->id }}"
+                                    value="{{ $promotion->id }}" @if ($loop->first) checked @endif>
+                            </div>
+                            <p>
+                                {{ $promotion->price }} € per {{ $promotion->duration }} ore di sponsorizzazione
+
+                            </p>
+                        </label>
                     </div>
-                </div>
-                
-                {{-- Prom 2--}}
-                <div class="col-4">
-                    <div class="card">
-                        <h2>Prom 2</h2>
-                        <div class="form-check-input d-flex">
-                            <input class="form-check-input" type="radio" name="promotion" id="promotion2" value="2">
-                            <label class="form-check-label" for="promotion2">2</label>
-                        </div>
-                        <p>
-                            Blablabla
-                        </p>
-                    </div>
-                </div>
-                
-                {{-- Prom 3--}}
-                <div class="col-4">
-                    <div class="card">
-                        <h2>Prom 3</h2>
-                        <div class="form-check-input d-flex">
-                            <input class="form-check-input" type="radio" name="promotion" id="promotion3" value="3">
-                            <label class="form-check-label" for="promotion3">3</label>
-                        </div>
-                        <p>
-                            Blablabla
-                        </p>
-                    </div>
-                </div>
+                @endforeach
+
             </div>
-    
-            
+
+
             <div id="dropin-container"></div>
-            <input type="submit" />
+            <input type="submit" class="btn btn-success" value="Procedi" />
             <input type="hidden" id="nonce" name="payment_method_nonce" />
         </form>
-        
-        
-        
+
+
+
     </div>
 @endsection
 
