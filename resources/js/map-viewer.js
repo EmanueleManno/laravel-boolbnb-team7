fadf11d5fce6d72665bd7092f2b5cf1b67e99123
Riccardo Basso <riccardo.basso2001@gmail.com>
2023-10-10 23:41:20 +0200

    edit map js

--- resources/js/map-viewer.js
+++ resources/js/map-viewer.js
@@ -3,25 +3,29 @@
 -------------------------------------------*/
 import tt from "@tomtom-international/web-sdk-maps"
 
+const mapButton = document.getElementById('map-button');
 
-// INIT
-const mapContainer = document.getElementById('map');
+mapButton.addEventListener('click', ()=>{
 
-if (mapContainer) {
-
-    const lat = mapContainer.dataset.latitude;
-    const lon = mapContainer.dataset.longitude;
-
-    const map = tt.map({
-        key: import.meta.env.VITE_TT_API_KEY,
-        container: mapContainer,
-        center: [
-            lon,
-            lat
-        ],
-        zoom: 12
-    });
-    map.addControl(new tt.NavigationControl());
-
-    const marker = new tt.Marker().setLngLat([lon, lat]).addTo(map);
-}
+    // INIT
+    const mapContainer = document.getElementById('map');
+    
+    if (mapContainer) {
+        
+        const lat = mapContainer.dataset.latitude;
+        const lon = mapContainer.dataset.longitude;
+        
+        const map = tt.map({
+            key: import.meta.env.VITE_TT_API_KEY,
+            container: mapContainer,
+            center: [
+                lon,
+                lat
+            ],
+            zoom: 12
+        });
+        map.addControl(new tt.NavigationControl());
+        
+        const marker = new tt.Marker().setLngLat([lon, lat]).addTo(map);
+    }
+})
