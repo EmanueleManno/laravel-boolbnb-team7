9e3575f7c27e0ab91fa83af7ca351c6780e8e396
l3on <l3onwebdev@gmail.com>
2023-10-10 18:52:52 +0200

    refactored js code

--- resources/views/admin/apartments/statistics.blade.php
+++ resources/views/admin/apartments/statistics.blade.php
@@ -4,132 +4,90 @@
 
 @section('main')
 
-<div class="container my-5">
-  <div class="row">
-    <!--Month Views-->
-    <div class="col-6">
-    <canvas id="month-views" data-statistics="{{ implode($month_views)}}"></canvas>
+    <div class="container my-5">
+        <div class="row">
+            <!--Month Views-->
+            <div class="col-6">
+                <canvas id="month-views" data-statistics="{{ implode($month_views) }}"></canvas>
+            </div>
+            <!--Month Messages-->
+            <div class="col-6">
+                <canvas id="month-messages" data-statistics="{{ implode($month_messages) }}"></canvas>
+            </div>
+            <!--Year Views-->
+            <div class="col-6">
+                <canvas id="year-views"></canvas>
+            </div>
+            <!--Year Messages-->
+            <div class="col-6">
+                <canvas id="year-messages"></canvas>
+            </div>
+        </div>
     </div>
-    <!--Month Messages-->
-    <div class="col-6">
-    <canvas id="month-messages" data-statistics="{{ implode($month_messages)}}"></canvas>
-    </div>
-    <!--Year Views-->
-    <div class="col-6">
-    <canvas id="year-views"></canvas>
-    </div>
-    <!--Year Messages-->
-    <div class="col-6">
-      <canvas id="year-messages"></canvas>
-    </div>
-  </div> 
-</div>
-  
+
 @endsection
 
 @section('scripts')
 
-<!--Import cdn ChartJS-->
-<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <!--Import cdn ChartJS-->
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
-<script>
-  //Year Views
-  const yearData = <?php echo json_encode($year_views) ?>;
-  const yearLabel = Object.keys(yearData);
-  const yearView = Object.values(yearData);
+    <script>
+        //*** FUNCTIONS ***//
 
-  //Year Messages
-  const yearDataMessages = <?php echo json_encode($year_messages) ?>;
-  const yearLabelMessages = Object.keys(yearDataMessages);
-  const yearMessages = Object.values(yearDataMessages);
+        const initGraph = (elem, title, labels, data) => {
+            const graph = new Chart(elem, {
+                type: 'bar',
+                data: {
+                    labels,
+                    datasets: [{
+                        label: title,
+                        data,
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                }
+            });
 
-  //Month Views & Messages
-  const month = document.getElementById('month-views');
-  const monthMessages = document.getElementById('month-messages');
-
-  //Month Views Graph
-  new Chart(month, {
-    type: 'bar',
-    data: {
-      labels: ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre' ,'Dicembre'],
-      datasets: [{
-        label: 'Visualizzazioni per mese',
-        data: month.dataset.statistics,
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
+            return graph;
         }
-      }
-    }
-  });
 
-  //Month Messages Graph
-  new Chart(monthMessages, {
-    type: 'bar',
-    data: {
-      labels: ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre', 'Ottobre', 'Novembre' ,'Dicembre'],
-      datasets: [{
-        label: 'Messaggi per mese',
-        data: monthMessages.dataset.statistics,
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
-        }
-      }
-    }
-  });
+        //*** INIT ***//
+        // Get DOM Elems
+        const viewsPerMonthsElem = document.getElementById('month-views');
+        const messagesPerMonthsElem = document.getElementById('month-messages');
+        const viewsPerYearsElem = document.getElementById('year-views');
+        const messagesPerYearsElem = document.getElementById('year-messages');
 
-  //Year Views Graph
-  const year = document.getElementById('year-views');
+        // Get Data
+        const yearData = <?php echo json_encode($year_views); ?>;
+        const yearLabel = Object.keys(yearData);
+        const yearView = Object.values(yearData);
 
-new Chart(year, {
-  type: 'bar',
-  data: {
-    labels: yearLabel,
-    datasets: [{
-      label: 'Visualizzazioni per anno',
-      data: yearView,
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
-      }
-    }
-  }
-});
+        const yearDataMessages = <?php echo json_encode($year_messages); ?>;
+        const yearLabelMessages = Object.keys(yearDataMessages);
+        const yearMessages = Object.values(yearDataMessages);
 
- //Year Messages Graph
- const yearMessagesGraph = document.getElementById('year-messages');
+        // Vars
+        const months = ['Gennaio', 'Febbraio', 'Marzo', 'Aprile', 'Maggio', 'Giugno', 'Luglio', 'Agosto', 'Settembre',
+            'Ottobre', 'Novembre', 'Dicembre'
+        ];
 
-new Chart(yearMessagesGraph, {
-  type: 'bar',
-  data: {
-    labels: yearLabelMessages,
-    datasets: [{
-      label: 'Messaggi per anno',
-      data: yearMessages,
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
-      }
-    }
-  }
-});
-</script>
+
+        //*** LOGIC ***//
+        // Init All Months Graphs
+        initGraph(viewsPerMonthsElem, 'Visualizzazioni per mese', months, viewsPerMonthsElem.dataset.statistics);
+        initGraph(messagesPerMonthsElem, 'Messaggi per mese', months, messagesPerMonthsElem.dataset.statistics);
+
+        // Init All Years Graphs
+        initGraph(viewsPerYearsElem, 'Visualizzazioni per anno', yearLabel, yearView);
+        initGraph(messagesPerYearsElem, 'Messaggi per anno', yearLabelMessages, yearMessages);
+    </script>
 
 @endsection
