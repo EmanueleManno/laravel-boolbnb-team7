4b2c6a6f6f078569d1627444adb72554012ccf7d
l3on <l3onwebdev@gmail.com>
2023-10-11 21:46:52 +0200

    fixed map render bug

--- resources/js/map-viewer.js
+++ resources/js/map-viewer.js
@@ -3,18 +3,18 @@
 -------------------------------------------*/
 import tt from "@tomtom-international/web-sdk-maps"
 
+// INIT
 const mapButton = document.getElementById('map-button');
+const mapContainer = document.getElementById('map');
 
-mapButton.addEventListener('click', ()=>{
+// Add Listener
+mapButton.addEventListener('click', () => {
 
-    // INIT
-    const mapContainer = document.getElementById('map');
-    
     if (mapContainer) {
-        
+
         const lat = mapContainer.dataset.latitude;
         const lon = mapContainer.dataset.longitude;
-        
+
         const map = tt.map({
             key: import.meta.env.VITE_TT_API_KEY,
             container: mapContainer,
@@ -25,7 +25,11 @@ mapButton.addEventListener('click', ()=>{
             zoom: 12
         });
         map.addControl(new tt.NavigationControl());
-        
+
         const marker = new tt.Marker().setLngLat([lon, lat]).addTo(map);
+
+        // Fix map size bug
+        setTimeout(() => { map.resize(); }, 200);
+
     }
 })
